44322699be1e11e930bf59495e65b3e3cc15c79f
mikailaAkeredolu <mikaila@tech552.com>
2023-05-22 14:38:05 -0400

    starter project for springboot

---
+++ src/main/java/com/mikaila/demochucknorrisapi/DemoChucknorrisApiApplication.java
@@ -0,0 +1,13 @@
+package com.mikaila.demochucknorrisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoChucknorrisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoChucknorrisApiApplication.class, args);
+	}
+
+}
